e616e4215d5a6a4bbdcd25f209baf9c2dffb3e1b
Andrew Chen <andrewchen@campus-010-200.ucdavis.edu>
2023-05-20 19:30:24 -0700

    Testing

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+export default App;
